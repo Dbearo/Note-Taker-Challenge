3559d625d5bb1efca89e7879a11c341b30dad3e3
Jude Hartmann <jude.hartmann@outlook.com>
2023-11-03 12:04:39 -0400

    added html routes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const path = require('path');
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
