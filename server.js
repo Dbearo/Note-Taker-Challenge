829dcc67fbcbbd8ac757b81d1e56f4dee457fe7e
Jude Hartmann <jude.hartmann@outlook.com>
2023-11-04 11:14:14 -0400

    moved html routes to it's own file

---
+++ server.js
@@ -0,0 +1,29 @@
+const { v4: uuidv4 } = require('uuid');
+
+const newUUID = uuidv4();
+console.log(newUUID);
+
+
+
+
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const path = require('path');
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
